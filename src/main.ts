89a9b6ac5094f6acca6716d724256f851f24e9a9
ThallesLana <thalles.lana1@gmail.com>
2025-09-12 10:43:56 -0300

    :bricks: ci: add Swagger documentation setup and configuration

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Economiza AI')
+    .setDescription(
+      'É uma API REST desenvolvida em **NestJS**, que tem como objetivo ajudar usuários a controlarem suas finanças pessoais de forma simples, eficiente e segura.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
