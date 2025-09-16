db9b7d21b5737f6d72218fb3221ae4e17468abd6
ThallesLana <thalles.lana1@gmail.com>
2025-09-16 10:27:46 -0300

    :books: docs:  add custom title to Swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,9 @@ async function bootstrap() {
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory, {
+    customSiteTitle: 'Economiza AI - API Documentation',
+  });
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
