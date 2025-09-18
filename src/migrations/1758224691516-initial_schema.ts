eec68866013f46fb3d93c9aa3ba3a7cd895c3f05
ThallesLana <thalles.lana1@gmail.com>
2025-09-18 16:47:16 -0300

    :sparkles: feat: implement initial database schema with user, transaction and category tables

---
+++ src/migrations/1758224691516-initial_schema.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitialSchema1758224691516 implements MigrationInterface {
+  name = 'InitialSchema1758224691516';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "Transactions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "description" character varying, "amount" numeric(10,2) NOT NULL, "type" "public"."Transactions_type_enum" NOT NULL, "categoryId" uuid, "transactionDate" TIMESTAMP NOT NULL, "userId" uuid NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_7761bf9766670b894ff2fdb3700" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "Categories" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "description" character varying, "userId" uuid NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_537b5c00afe7427c4fc9434cd59" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "Users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying, "phone" character varying NOT NULL, "password" character varying NOT NULL, "status" "public"."Users_status_enum" NOT NULL DEFAULT 'active', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_3c3ab3f49a87e6ddb607f3c4945" UNIQUE ("email"), CONSTRAINT "UQ_f0444b8b5c111257c300932ae06" UNIQUE ("phone"), CONSTRAINT "PK_16d4f7d636df336db11d87413e3" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "Transactions" ADD CONSTRAINT "FK_f01450fedf7507118ad25dcf41e" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "Transactions" ADD CONSTRAINT "FK_7fd01b115723d6d88b160fa740a" FOREIGN KEY ("categoryId") REFERENCES "Categories"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "Categories" ADD CONSTRAINT "FK_8111f8d3e5f088377ee14b36a9f" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "Categories" DROP CONSTRAINT "FK_8111f8d3e5f088377ee14b36a9f"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "Transactions" DROP CONSTRAINT "FK_7fd01b115723d6d88b160fa740a"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "Transactions" DROP CONSTRAINT "FK_f01450fedf7507118ad25dcf41e"`,
+    );
+    await queryRunner.query(`DROP TABLE "Users"`);
+    await queryRunner.query(`DROP TABLE "Categories"`);
+    await queryRunner.query(`DROP TABLE "Transactions"`);
+  }
+}
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -3,17 +3,15 @@ import { config } from 'dotenv';
 
 config();
 
-export const AppDataSource = new DataSource({
+export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD?.toString(),
   database: process.env.DB_NAME,
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/migrations/*.ts'],
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
   synchronize: false,
-  logging: process.env.NODE_ENV !== 'production' ? true : ['error', 'warn'],
+  logging: false,
 });
-
-export default AppDataSource;
